8f2b2fa9f9bac99a353b43fbad280761bafefde4
Navanee <navaneekrish164@gmail.com>
2022-09-16 16:53:17 +0530

    js

---
+++ main.js
@@ -0,0 +1,25 @@
+var date = new Date(1999,9,7,12,0,9);
+console.log(date);
+
+
+var x = new Date();
+console.log(x.getDate());
+
+console.log(x.getDay());
+
+console.log(x.getYear());
+
+console.log(x.getFullYear());
+
+console.log(x.getHours()*2);
+
+console.log(x.getMilliseconds());
+
+console.log(x.getMinutes());
+
+console.log(x.getMonth()+1);
+
+console.log(x.getSeconds());
+
+console.log(x.getTime());
+
